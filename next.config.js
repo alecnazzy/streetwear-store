c7f8a9b7179b0c2939d7cb0290f42be65227130f
alecnazworth <alecnazworthalt@gmail.com>
2022-12-15 17:08:24 -0500

    - trying to fix for Vercel deployment **

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "user-images.githubusercontent.com",
-          port: '3000',
-          pathname: "/90207985/**"},
-      ],
+      domains: ['user-images.githubusercontent.com'],
     },
-  }
-  
+  };
